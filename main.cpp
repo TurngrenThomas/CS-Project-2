700ce6fabb20085c3d9662459fcd0042f33f1d78
TurngrenThomas <89604178+TurngrenThomas@users.noreply.github.com>
2022-03-03 23:50:44 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,128 +1,118 @@
-#include <stdio.h>
-#include <stdlib.h>  // for atoi
-#include <cstring>   // for strlen
-#include <algorithm> // for remove
+//Name: Thomas Turngren
+//Date:3/3/22
+//class: CS 219
+//description: completes hexadecimal operations
 
-struct _ADD_SETTINGS {
-    int spaces = 0;
-    int spaces_i = 0;
-    int spaces_i2 = 0; // quick fix
-    int num1=0;
-    int num2=0;
-    char num_1[100];
-    char num_2[100];
 
-    void reset() {
-        spaces = 0;
-        spaces_i = 0;
-        spaces_i2 = 0;
-        num1 = 0;
-        num2 = 0;
-        memset(&(num_1), ' ', strlen(num_1));
-        memset(&(num_2), ' ', strlen(num_2));
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <stdint.h>
+
+int main(){
+
+    const std::string filename = "Programming-Project-1.txt";
+
+    std::vector<std::string> operation,stroperand1, stroperand2;
+    std::vector<unsigned int> operand1, operand2;
+    
+    std::ifstream fileReader(filename, std::ios::in);
+
+    while(fileReader.good()){
+        
+        std::string tempoper;
+        std::string tempop1, tempop2;
+
+        unsigned int tempoper1, tempoper2;
+        fileReader >> tempoper >> tempop1 >> tempop2;
+        stroperand1.push_back(tempop1);
+        stroperand2.push_back(tempop2);
+
+        std::istringstream(tempop1) >> std::hex >> tempoper1;
+        std::istringstream(tempop2) >> std::hex >> tempoper2;
+
+        operation.push_back(tempoper);
+        operand1.push_back(tempoper1);
+        operand2.push_back(tempoper2);
+        if (fileReader.eof()) break;
     }
 
-} Add_Settings;
+    for (int i = 0; operation.size() > i; i++){
+        if (operation.at(i) == "ADD"){
 
-enum CONTROL_CODES{
-    READ=0,
-    ADD=1,
-    SUB,
-    MULT,
-    DIV
-};
+            uint32_t total = operand1.at(i) + operand2.at(i);
+            std::ostringstream hexoutput;
+            hexoutput << std::hex << total;
+            std::string result = hexoutput.str();
+            std::cout << stroperand1.at(i) << " + " << stroperand2.at(i) << " = 0x" << result << std::endl;
 
-void removeSpaces(char* s) {
-    *std::remove(s, s + strlen(s), ' ') = 0;
-    *std::remove(s, s + strlen(s), '\n') = 0;
-}
+        }
+        if (operation.at(i) == "AND"){
+            uint32_t total = operand1.at(i) & operand2.at(i);
+            std::ostringstream hexoutput;
+            hexoutput << std::hex << total;
+            std::string result = hexoutput.str();
+            std::cout << stroperand1.at(i) << " & " << stroperand2.at(i) << " = 0x" << result << std::endl;
+        }
+        if (operation.at(i) == "ORR"){
+            uint32_t total = operand1.at(i) | operand2.at(i);
+            std::ostringstream hexoutput;
+            hexoutput << std::hex << total;
+            std::string result = hexoutput.str();
+            std::cout << stroperand1.at(i) << " | " << stroperand2.at(i) << " = 0x" << result << std::endl;
 
-int is_instruction(char c, int count) {   
-
-    if (c == 'A' && count == 0) 
-        return 1;
-    if (c == 'D' && count == 1) 
-        return 1;
-    if (c == 'D' && count == 2) 
-        return 1;
-
-    return 0;
-}
-
-void INS_ADD() {
-    
-    printf("BNum1: %s   ", Add_Settings.num_1);
-    printf("BNum2: %s\n", Add_Settings.num_2);
-    removeSpaces(Add_Settings.num_1); // Sanitize
-    removeSpaces(Add_Settings.num_2);
-    strncpy(Add_Settings.num_1, Add_Settings.num_1+2, strlen(Add_Settings.num_1));
-    strncpy(Add_Settings.num_2, Add_Settings.num_2+2, strlen(Add_Settings.num_2));
-    sscanf(Add_Settings.num_1, "%x", &Add_Settings.num1);
-    sscanf(Add_Settings.num_2, "%x", &Add_Settings.num2);
-
-    printf("ANum1: %s   ", Add_Settings.num_1);
-    printf("ANum2: %s\n", Add_Settings.num_2);
-
-    printf("(Number 1: %d) + ", Add_Settings.num1);
-    printf("(Number 2: %d) = ", Add_Settings.num2);
-    printf("%d | Hex: %x\n", Add_Settings.num1 + Add_Settings.num2, Add_Settings.num1 + Add_Settings.num2);
-    
-    Add_Settings.reset();
-}
-
-int main() {
-    FILE *f;
-    char file_chars[100];
-    int i=0, count = 0, control_code = READ;
-
-    f=fopen("Programming-Project-1.txt","r");
-    if (f==NULL){
-        printf("file doesnt exist?!\n");
-        return 1;
-    }
-
-    int fCode = fscanf(f, "%c", &file_chars[i]);
-    while (fCode != EOF) {
-        if (control_code == READ) {
-            if (is_instruction(file_chars[i], count)) {
-                count++;
-                if (count == 3) {
-                   control_code = ADD;
-                   count=0;
-                }
-            } else {
-                count = 0;
+        }
+        if (operation.at(i) == "SUB"){
+            uint32_t total = operand1.at(i) - operand2.at(i);
+            std::ostringstream hexoutput;
+            hexoutput << std::hex << total;
+            std::string result = hexoutput.str();
+            std::cout << stroperand1.at(i) << " - " << stroperand2.at(i) << " = 0x" << result << std::endl;
+        }
+        if (operation.at(i) == "ASR"){
+            uint32_t total = operand1.at(i);
+            for(int i = 0; i < operand2.at(i); i++){
+                total = total * 2;
+                total = total / 2;
             }
-        } else if (control_code == ADD) {
-            if (Add_Settings.spaces == 1) {
-                printf("B: Add_Settings[%d] = %c\n", Add_Settings.spaces_i, Add_Settings.num_1[Add_Settings.spaces_i]);
-                Add_Settings.num_1[Add_Settings.spaces_i++] = file_chars[i];
-                printf("A: Add_Settings[%d] = %c\n", Add_Settings.spaces_i, Add_Settings.num_1[Add_Settings.spaces_i-1]);
-                printf("1 Char: %c\n", file_chars[i]);
-                printf("FILE I: %c\n", file_chars[i]);
-            }
+            std::ostringstream hexoutput;
+            hexoutput << std::hex << total;
+            std::string result = hexoutput.str();
+            std::cout << stroperand1.at(i) << " shifted right by " << stroperand2.at(i) << " spaces = 0x" << result << std::endl;
+        }
+        if (operation.at(i) == "LSR"){
+            uint32_t total = operand1.at(i) >> operand2.at(i);
+            std::ostringstream hexoutput;
+            hexoutput << std::hex << total;
+            std::string result = hexoutput.str();
+            std::cout << stroperand1.at(i) << " shifted right by " << stroperand2.at(i) << " spaces = 0x" << result << std::endl;
+        }
+        if (operation.at(i) == "LSL"){
+            uint32_t total = operand1.at(i) << operand2.at(i);
+            std::ostringstream hexoutput;
+            hexoutput << std::hex << total;
+            std::string result = hexoutput.str();
+            std::cout << stroperand1.at(i) << " shifted left by " << stroperand2.at(i) << " spaces = 0x" << result << std::endl;
+        }
+        if (operation.at(i) == "NOT"){
+            uint32_t total = ~(operand1.at(i));
+            std::ostringstream hexoutput;
+            hexoutput << std::hex << total;
+            std::string result = hexoutput.str();
+            std::cout << "~" << stroperand1.at(i) << " = 0x" << result << std::endl;
 
-            if (Add_Settings.spaces == 2) {
-                if (file_chars[i] == '\n' || file_chars[i] == ' ') {
-                    // Quick Fix
-                    control_code = READ;
-                    INS_ADD();
-                    printf("===================================================================\n\n");
-                } else {
-                    Add_Settings.num_2[Add_Settings.spaces_i2++] = file_chars[i];
-                }
-            }
+        }
+        if (operation.at(i) == "XOR"){
+            uint32_t total = operand1.at(i) ^ operand2.at(i);
+            std::ostringstream hexoutput;
+            hexoutput << std::hex << total;
+            std::string result = hexoutput.str();
+            std::cout << stroperand1.at(i) << " ^ " << stroperand2.at(i) << " = 0x" << result << std::endl;
 
-            if (file_chars[i] == ' ' || file_chars[i] == '\n' || fCode == EOF) {
-                Add_Settings.spaces++;
-            }
         }
 
-        i++;
-        fCode = fscanf(f, "%c", &file_chars[i]);
     }
-
-    fclose(f);
-    printf("END\n");
+    
     return 0;
 }
